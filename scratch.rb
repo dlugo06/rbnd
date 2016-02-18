f362e16ee3a2568ef4edd986bc9f86e082b1ad05
Diego Lugo <diego.lugo.06@gmail.com>
2016-02-18 15:57:34 -0800

    merger

--- scratch.rb
+++ scratch.rb
@@ -5,7 +5,10 @@ end
 div_by_five?(20)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of b2ec9b2... add add_5 method
 =======
 >>>>>>> parent of b2ec9b2... add add_5 method
 def method_name
